3b3a9158b7b3a53f6918d8d8864248549fd5adb3
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-09-16 23:21:46 +0200

    create src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParkingsComponent } from './parkings/parkings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParkingsComponent
   ],
   imports: [
     BrowserModule
