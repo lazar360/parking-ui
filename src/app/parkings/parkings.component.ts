8fae6860a75a10b63978a35e8374eb293646efa2
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-09-19 21:48:10 +0200

    create bouchon parkings

--- src/app/parkings/parkings.component.ts
+++ src/app/parkings/parkings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ParkingInfo } from '../parkinginfo';
 
 @Component({
   selector: 'app-parkings',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParkingsComponent implements OnInit {
 
+  parkings!: ParkingInfo[];
   constructor() { }
 
   ngOnInit(): void {
+
+    let parking1: ParkingInfo ={
+      id: 1,
+      nom: "Parking 1",
+      nbPlacesDispo: 150,
+      nbPlacesTotal: 200,
+      statut: "OUVERT",
+      heureMaj: "20h22"
+    }
+
+    let parking2: ParkingInfo ={
+      id: 2,
+      nom: "Parking 2",
+      nbPlacesDispo: 168,
+      nbPlacesTotal: 302,
+      statut: "ABONNES",
+      heureMaj: "21h22"
+    }
+    this.parkings.push(parking1);
+    this.parkings.push(parking2);
   }
 
 }
