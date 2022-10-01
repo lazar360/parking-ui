41feb0c8142c47b0124f2ea321cad4f18d07fc24
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-10-01 09:44:29 +0200

    update src/app/parkings/parkings.component.ts

--- src/app/parkings/parkings.component.ts
+++ src/app/parkings/parkings.component.ts
@@ -10,6 +10,8 @@ import { ParkingInfo } from '../parkinginfo';
 export class ParkingsComponent implements OnInit {
 
   parkings!: ParkingInfo[];
+  isLoaded: boolean = false;
+
   constructor(private parkingService : ParkingService) { }
 
   ngOnInit(): void {
@@ -17,8 +19,21 @@ export class ParkingsComponent implements OnInit {
     this.parkingService.getParkings().subscribe(
       reponse => {
         this.parkings = reponse;
+        this.isLoaded = true;
       }
     );
+  }
+
+  calculStyleStatut(parking:ParkingInfo){
+    if(parking.statut === 'OUVERT'){
+      return {color:'green'}
+    }else if(parking.statut === 'ABONNES'){
+      return {color:'orange'}
+    }else if(parking.statut === 'FERME'){
+      return {color:'red'}
+    }else{
+      return {'font-style' : 'italic'}
+    }
   }
 
 }
